a9429a8cfb71e560205d54e3837ea4f3e7f37170
huiyeong <81677222+almond-flavoured@users.noreply.github.com>
2023-08-24 18:00:40 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import words from './wordList.json';
+import './App.css';
+import { HangmanDrawing } from './HangmanDrawing';
+import { Hangmanword } from './HangmanWord';
+import { Keyboard } from './Keyboard';
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return 'test';
+    // return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>(['g']);
+  const inCorrectLetters = guessedLetters.filter((letter) => {
+    return !wordToGuess.includes(letter);
+  });
+
+  return (
+    <>
+      <div
+        style={{
+          maxWidth: '800px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '2rem',
+          margin: '0 auto',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            fontSize: '2rem',
+            textAlign: 'center',
+          }}
+        >
+          Lose Win
+        </div>
+        <HangmanDrawing numberOfGuesses={inCorrectLetters.length} />
+        <Hangmanword />
+        <div
+          style={{
+            alignSelf: 'stretch',
+          }}
+        >
+          <Keyboard />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
